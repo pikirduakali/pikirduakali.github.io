faa1d8938513526d3df0e9b9bd21151b06809041
pikirduakali <pramsuwandi@gmail.com>
2026-07-25 07:25:05 +0700

    update-0725

--- gitpush.bat
+++ gitpush.bat
@@ -1,7 +1,11 @@
 @echo off
 cd /d "%~dp0"
-set msg=%1
-if "%msg%"=="" set msg=update
+for /f "tokens=*" %%a in ('powershell -Command "Add-Type -AssemblyName Microsoft.VisualBasic; $m=[Microsoft.VisualBasic.Interaction]::InputBox('Enter commit message:', 'Git Push', 'update'); if ($m) { Write-Output $m } else { exit 1 }"') do set msg=%%a
+if "%msg%"=="" (
+  echo Cancelled.
+  pause
+  exit /b
+)
 git add -A
 git commit -m "%msg%"
 git push
